9fe69eb895e1315609a1254e5c524d64622ee57f
Usman Jamshed <usman.jamshedk@gmail.com>
2025-08-04 00:15:16 -0400

    Add more molecule smiles to list

--- script/molecule.py
+++ script/molecule.py
@@ -4,10 +4,24 @@ from rdkit.Chem import Lipinski
 
 # Sample list of SMILES strings
 SMILES_LIST = [
-    "CC(=O)OC1=CC=CC=C1C(=O)O",      # Aspirin
-    "CC1=CC(=O)NC(C)=C1",            # Paracetamol
-    "CCN(CC)CCCC(C)NC1=NC=NC2=C1C=CN2",  # Caffeine
-    "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"   # Theobromine
+    "CC(=O)OC1=CC=CC=C1C(=O)O",                 # Aspirin
+    "CC1=CC(=O)NC(C)=C1",                       # Paracetamol
+    "CCN(CC)CCCC(C)NC1=NC=NC2=C1C=CN2",         # Caffeine
+    "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"              # Theobromine
+    "CN1CCC(CC1)C2=CN=CC=C2",                   # Nicotine
+    "CC(C)NCC(O)COC1=CC=CC=C1",                 # Propranolol
+    "CN(C)C(=O)C1=CC=CC=C1Cl",                  # Lidocaine
+    "CC(C)(C)OC(=O)N1CCCC1C(=O)NC2=CC=CC=C2",   # Atorvastatin fragment
+    "CCOC(=O)C1=CC=CC=C1Cl",                    # Ethyl 4-chlorobenzoate
+    "CNC1=CC=CC=C1Cl",                          # Chlorpheniramine
+    "CC(C)COC(=O)C1=CC=CC=C1",                  # Ibuprofen
+    "CC(C)NCC(O)COC1=CC=CC=C1Cl",               # Metoprolol
+    "CC(C)N1CCC(CC1)NC2=NC=NC3=C2C=CN3",        # Theophylline derivative
+    "CN1C=NC2=C1C(=O)N(C(=O)N2C)C",             # Repeated (Theobromine)
+    "CCOC(=O)C1=CC=CC=C1OC",                    # Methyl salicylate
+    "COC1=CC=CC=C1OC",                          # Anisole
+    "CC1=CC=C(C=C1)C(C)C(=O)O",                 # Naproxen
+    "COC(=O)C1=CC=CC=C1Cl",                     # Methyl 4-chlorobenzoate
 ]
 
 # Substructure SMARTS for aromatic ring
